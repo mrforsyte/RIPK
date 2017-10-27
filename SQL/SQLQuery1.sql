07ec5974f593f1225729dd38148711b2654248ce
Ross Petrenko <petrenko.rostislav@gmail.com>
2017-10-27 14:19:18 +0300

    Шлак

---
+++ SQL/SQLQuery1.sql
@@ -0,0 +1,43 @@
+CREATE TABLE TEACHER 
+(
+teacher char(10),
+teacher_name varchar(50),
+pulpit char (10)
+)
+
+insert into xteacher (teacher, teacher name, pulpit)
+values ('УРБ', 'Урбанович Павел Павлович', 'ИСиТ')
+
+insert into xteacher (teacher, teacher name, pulpit)
+values ('ДИК', 'Дикий Сергей Степанович', 'ИСиТ')
+
+insert into xteacher (teacher, teacher name, pulpit)
+values ('БЫК', 'Быков Иван Иванович', 'ИСиТ')
+
+
+insert into xteacher (teacher, teacher name, pulpit)
+values ('МАГ', 'Магнитов Гасян Аганесянович', 'ИСиТ')
+
+insert into xteacher (teacher, teacher name, pulpit)
+values ('УРБ', 'Урбанович Павел Павлович', 'ИСиТ')
+
+insert into xteacher (teacher, teacher name, pulpit)
+values ('УРБ', 'Урбанович Павел Павлович', 'ИСиТ')
+
+insert into xteacher (teacher, teacher name, pulpit)
+values ('УРБ', 'Урбанович Павел Павлович', 'ИСиТ')
+
+insert into xteacher (teacher, teacher name, pulpit)
+values ('УРБ', 'Урбанович Павел Павлович', 'ИСиТ')
+
+insert into xteacher (teacher, teacher name, pulpit)
+values ('УРБ', 'Урбанович Павел Павлович', 'ИСиТ')
+
+insert into xteacher (teacher, teacher name, pulpit)
+values ('УРБ', 'Урбанович Павел Павлович', 'ИСиТ')
+
+insert into xteacher (teacher, teacher name, pulpit)
+values ('УРБ', 'Урбанович Павел Павлович', 'ИСиТ')
+
+select count (*) from xteacher 
+
